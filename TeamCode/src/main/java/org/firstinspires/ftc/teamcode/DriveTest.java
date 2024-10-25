c4a3e32689ed29c2a201d808d39d57b1d616ab41
Me <123142178+epic-person-on@users.noreply.github.com>
2024-10-25 08:35:07 -0700

    Create DriveTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+ 
+@Autonomous(name="Java: Drive Test", group="Linear OpMode")
+
+public class DriveTest extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    @Override
+    public void runOpMode() {
+
+
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "MotorWhite");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "MotorGrey");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "MotorRed");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "MotorBlue");
+
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+            leftBackDrive.setPower(-0.5);
+            rightBackDrive.setPower(0.5);
+            rightFrontDrive.setPower(-0.5);
+            leftFrontDrive.setPower(-0.5);
+           // sleep(500);
+
+            //leftFrontDrive.setPower(0);
+            //leftBackDrive.setPower(0);
+            //rightFrontDrive.setPower(0);
+            //rightBackDrive.setPower(0);
+        }
+    }
+}
+    
